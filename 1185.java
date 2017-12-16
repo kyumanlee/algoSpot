2ac2461e69501c422ee9f7b2a216b6f1e70e8a34
kyumanlee <kmlee@konyang.ac.kr>
2017-12-16 20:51:14 +0900

    Create 1185.java

---
+++ 1185.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+
+public class Main {
+
+	public static void main ( String args[]) {
+	
+		Scanner sc = new Scanner ( System.in );
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		
+		Queue<Integer> queue = new LinkedList<Integer> ();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append('<');
+		
+		for ( int i = 1 ;i <= n; i++ ) {
+			queue.add(i);
+		}
+		
+		for ( int i = 0 ; i < n - 1; i++ ) {
+			for ( int j = 0 ; j < m - 1; j++ ) {
+				queue.add(queue.poll());
+			}
+			sb.append(queue.poll() + ", ");
+		}
+		sb.append(queue.poll() + ">");
+		
+		System.out.println(sb);
+	}
+}
